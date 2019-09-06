c163584c9fcef652f6f38f2f07115ddea535bc7a
Parth <parth1493@gmail.com>
2019-09-06 04:39:47 -0400

    added Login screen     - Added animation on screen change activity added Main screnn     - Added alert dialog box and sharedPreference

--- app/src/main/java/com/parth/navigationviews/MainActivity.java
+++ app/src/main/java/com/parth/navigationviews/MainActivity.java
@@ -1,13 +1,50 @@
 package com.parth.navigationviews;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parth.navigationviews.utils.PreferenceKeys;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"on Create started");
+
+        firstTimeLogin();
+    }
+
+    private void firstTimeLogin() {
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstTimeLogin = sharedPreferences.getBoolean(PreferenceKeys.FIRST_TIME_LOGIN,true);
+
+        if(isFirstTimeLogin) {
+            Log.d(TAG,"First time login");
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage(getString(R.string.first_time_user_message));
+            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.d(TAG,"OK clicked");
+                    SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+                    sharedPreferencesEditor.putBoolean(PreferenceKeys.FIRST_TIME_LOGIN,false);
+                    sharedPreferencesEditor.commit();
+                    dialog.dismiss();
+                }
+            });
+            alertDialogBuilder.setIcon(R.drawable.tabian_dating);
+            alertDialogBuilder.setTitle(" ");
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
     }
 }
---
+++ app/src/main/java/com/parth/navigationviews/utils/Resources.java
@@ -0,0 +1,9 @@
+package com.parth.navigationviews.utils;
+
+
+import com.parth.navigationviews.R;
+
+public class Resources {
+
+    public static final int BACKGROUND_HEARTS = R.drawable.heart_background;
+}
